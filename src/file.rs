7076dcc4a0dbcbf7efb9119ed8f6eca2234a372d
Nathan Kleyn <nathan@nathankleyn.com>
2018-08-16 10:46:16 +0100

    Opt-in to 2018 edition preview.

--- src/file.rs
+++ src/file.rs
@@ -11,7 +11,7 @@ struct File {
 /// Given a single specific gitignore style file, allow matching against
 /// the rules within that file.
 impl File {
-    fn new<P: AsRef<Path>>(path: P) -> Result<File> {
+    fn new<P: AsRef<Path>>(_path: P) -> Result<File> {
         unimplemented!();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,11 @@
 //! `core.excludesFile` (the user excludes file); rather, it will only load patterns specified in
 //! the `.gitignore` file in the given directory.
 
+#![feature(rust_2018_preview)]
+
 // FIXME: Remove this when we're done developing!
 #![allow(dead_code)]
+
 #![recursion_limit = "1024"]
 #![cfg_attr(all(test, feature = "benchmarks"), feature(test))]
 
