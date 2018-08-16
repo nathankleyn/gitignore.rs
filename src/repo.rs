5693d7b7c13b7eaa7f72322e74fb9462314f23de
Nathan Kleyn <nathan@nathankleyn.com>
2018-08-16 10:46:16 +0100

    Opt-in to 2018 edition preview.

--- src/repo.rs
+++ src/repo.rs
@@ -9,7 +9,7 @@ struct Repo {
 /// Given the path to a Git repository, load up all of the ignore files in the
 /// usual Git heirachy and allow checking of ignore status against all of them.
 impl Repo {
-    fn new(path: PathBuf) -> Result<Repo> {
+    fn new(_path: PathBuf) -> Result<Repo> {
         unimplemented!();
     }
 }
